ef85918c7de77f1cd1a07278a551f1c473e00e6c
ldwql@hotmail.com <ldwql@hotmail.com>
2017-08-31 19:15:59 +0800

    44

--- DAL/Dal_ShopList.cs
+++ DAL/Dal_ShopList.cs
@@ -9,16 +9,20 @@ namespace DAL
 {
     public class Dal_ShopList
     {
+        /// <summary>
+        /// 获取店铺信息
+        /// </summary>
+        /// <returns></returns>
         public List<Model.M_ShopList.Return> ShopList()
         {
             return Common.Config.StartSqlSugar<List<Model.M_ShopList.Return>>((db) =>
             {
                 return db.Queryable<TaoBaoInfo>()
-                                .Select<Model.M_ShopList.Return>(a => new Model.M_ShopList.Return
-                                {
-                                    ID = a.ID,
-                                    Name = a.Name
-                                }).ToList();
+                         .Select<Model.M_ShopList.Return>(a => new Model.M_ShopList.Return
+                         {
+                             ID = a.ID,
+                             Name = a.Name
+                         }).ToList();
             });
 
         }
